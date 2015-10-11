4eb58ba8faf800979b1b02e6852443fd9c312db9
lucia33 <liu_chang2010@hotmail.com>
2015-10-11 12:27:52 -0400

    changed nabber and build some skeletons

--- routes/index.js
+++ routes/index.js
@@ -16,4 +16,9 @@ router.get('/contact', function(req, res, next) {
 	res.render('contact', { title: 'Contact Me'});
 });
 
+// GET projects page
+router.get('/projects', function(req, res, next) {
+	res.render('projects', { title: 'Projects'});
+});
+
 module.exports = router;
